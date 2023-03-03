c8f07a8772b38aa3deab45ca0edbf91ab45be590
Bogdan Preda <bogdan.preda@themeisle.com>
2023-03-03 19:45:39 +0200

    chore: sync changes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Hackathon AI Translate
+
+
+### Issues
++ Can it translate from other languages than English?
++ Nested tags?
++ How to ensure token limit?
++ API rate limit
++ Caching per translation
++ When to query the API?
--- tests/test.js
+++ tests/test.js
@@ -8,14 +8,35 @@ const openai = new OpenAIApi(configuration);
 
 //Create a jest test
 test('test', async () => {
+    const translatePrompt = "Translate this into French, preserve formatting: ";
+
+    const tokenList = [
+        {
+            "original": "Going to the store to get some milk.",
+            "translated": "Going to the store to get some milk.",
+        },
+        {
+            "original": "Going to the store to get some water.",
+            "translated": "Going to the store to get some water.",
+        },
+    ];
+
+    const promptArray = tokenList.map((token) => {
+            return `${translatePrompt}${token.original}`;
+    });
+
+    console.log(promptArray);
+
+
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Translate this into French, preserve formatting : Going to the store to get some milk.",
+        //prompt: "Translate this into French, preserve formatting : Going to the store to get some milk.",
+        prompt: promptArray,
         temperature: 0.3,
         max_tokens: 100,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
     });
-    console.log(response.data.choices[0].text);
+    console.log(response.data.choices);
 });
