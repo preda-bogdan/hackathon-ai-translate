9bd36bb358d441ac31ba3a26dd89acea0c4e1b82
Bogdan Preda <bogdan.preda@themeisle.com>
2023-03-03 17:17:13 +0200

    chore: plugin ehader

---
+++ hackathon-ai-translate.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       AI Translate
+ * Description:       This plugin is a hackathon project to test the AI translation capabilities.
+ * Version:           2.5.3
+ * Plugin URI:        https://ai-translate.test
+ * Author:            DreamTeam
+ * License:           MIT
+ * License URI:       https://opensource.org/license/mit
+ * Text Domain:       hack-ai-translate
+ * Domain Path:       /languages
+ * Requires PHP:      7.4
+ *
+ * @package HackathonAITranslate
+ */
+error_log( var_export( 'Loaded', true ) );
